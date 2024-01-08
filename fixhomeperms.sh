22f88200643695df3a8b0305f4c6cd973010b563
Martin Mielke <martinm@rsysadmin.com>
2024-01-08 17:06:43 +0100

    Minor changes

--- fixhomeperms.sh
+++ fixhomeperms.sh
@@ -6,9 +6,9 @@
 # quick and dirty script to fix permissions for all files and directories under $HOME
 # 
 # the following is assumed to be true for the final results:
-#       * directories: rwxr-x-r-x (octal: 755)
-#       * regular files: rw-r--r-- (octal: 644)
-#       * executable files: rwxr-xr-x (octal: 755)
+#       * directories: rwxr-x-r-x (octal: 0755)
+#       * regular files: rw-r--r-- (octal: 0644)
+#       * executable files: rwxr-xr-x (octal: 0755)
 #           - Shell scripts (.sh)
 #           - Perl files (.pl)
 #           - Python files (.py)
@@ -22,16 +22,16 @@
 
 usage() # option -h
 {
-    echo "$(basename $0) by Martin Mielke <martin@mielke.com>"
-        echo -e "Usage:\t$(basename $0) [-h] [-d] [-n] [-s] [-p] [-P] [-r] [-R] [-a]"
+    echo "$(basename $0) by Martin Mielke <martinm@rsysadmin.com>"
+        echo -e "\nUsage:\t$(basename $0) [-h] [-d] [-n] [-s] [-p] [-P] [-r] [-R] [-a]"
         echo -e "\t-h  Prints this help."
-        echo -e "\t-d  Fix permissions on directories (755)."
-        echo -e "\t-n  Fix permissions on non-exec files (644)."
-        echo -e "\t-s  Fix permissions on shell script files (755)."
-        echo -e "\t-p  Fix permissions on Perl files (755)."
-        echo -e "\t-P  Fix permissions on Python files (755)."
-        echo -e "\t-r  Fix permissions on Ruby files (755)."
-        echo -e "\t-R  Fix permissions on RUN files (755)."        
+        echo -e "\t-d  Fix permissions on directories (0755)."
+        echo -e "\t-n  Fix permissions on non-exec files (0644)."
+        echo -e "\t-s  Fix permissions on shell script files (0755)."
+        echo -e "\t-p  Fix permissions on Perl files (0755)."
+        echo -e "\t-P  Fix permissions on Python files (0755)."
+        echo -e "\t-r  Fix permissions on Ruby files (0755)."
+        echo -e "\t-R  Fix permissions on RUN files (0755)."        
         echo -e "\t-a  Fix permissions on all executable files (.sh, .pl, .py, .rb, .run)."
         echo -e "\t-A  Fix all in one step, including directories and non-exec files."
         exit 1
@@ -52,45 +52,45 @@ function finishMessage {
 }
 
 function fixDirectories {   # option -d
-    echo -e " [$(date +%_H:%M:%S)] == Fixing directory permissions (chmod 755) ... \c"
-    find $HOME/ -type d -print0 | xargs -0 -n1024 -P0 chmod 755
+    echo -e " [$(date +%_H:%M:%S)] == Fixing directory permissions (chmod 0755) ... \c"
+    find $HOME/ -type d -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 }
 
 function fixNonExecFiles {  # option -n
-    echo -e " [$(date +%_H:%M:%S)] == Fixing non-exec files permissions (chmod 644) ... \c"
-    find $HOME/ -type f \( -not -name '*.sh' -not -name '*.pl' -not -name '*.py' -not -name '*.rb' -not -name '*.run' -not -path '*/\.*' \) -print0 | xargs -0 -n1024 -P0 chmod 644
+    echo -e " [$(date +%_H:%M:%S)] == Fixing non-exec files permissions (chmod 0644) ... \c"
+    find $HOME/ -type f \( -not -name '*.sh' -not -name '*.pl' -not -name '*.py' -not -name '*.rb' -not -name '*.run' -not -path '*/\.*' \) -print0 | xargs -0 -n1024 -P0 chmod 0644
     returnStatus
 }
 
 function fixShellFiles {    # option -s
     echo -e "[$(date +%_H:%M:%S)] == Fixing permissions for Shell scripts... \c"
-    find $HOME/ -type f -name '*.sh' -print0 | xargs -0 -n1024 -P0 chmod 755
+    find $HOME/ -type f -name '*.sh' -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 
 }
 
 function fixPerlFiles {     # option -p
     echo -e "[$(date +%_H:%M:%S)] == Fixing permissions for Perl files... \c"
-    find $HOME/ -type f -name '*.pl' -print0 | xargs -0 -n1024 -P0 chmod 755
+    find $HOME/ -type f -name '*.pl' -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 }
 
 function fixPythonFiles {   # option -P
     echo -e "[$(date +%_H:%M:%S)] == Fixing permissions for Python files... \c"
-    find $HOME/ -type f -name '*.py' -print0 | xargs -0 -n1024 -P0 chmod 755
+    find $HOME/ -type f -name '*.py' -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 }
 
 function fixRubyFiles {     # option -r
     echo -e "[$(date +%_H:%M:%S)] == Fixing permissions for Ruby files... \c"
-    find $HOME/ -type f -name '*.rb' -print0 | xargs -0 -n1024 -P0 chmod 755
+    find $HOME/ -type f -name '*.rb' -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 }
 
 function fixRunFiles {      # option -R
     echo -e "[$(date +%_H:%M:%S)] == Fixing permissions for RUN files... \c"
-    find $HOME/ -type f -name '*.run' -print0 | xargs -0 -n1024 -P0 chmod 755
+    find $HOME/ -type f -name '*.run' -print0 | xargs -0 -n1024 -P0 chmod 0755
     returnStatus
 }
 
